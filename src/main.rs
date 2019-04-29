ebdfc1d5728050418ffcfa904fa5fe77176f959b
mass10 <mass10.github@gmail.com>
2019-04-29 11:02:05 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+
+use rocket_contrib::{serve::StaticFiles};
 
 #[get("/")]
 fn index() -> &'static str {
@@ -16,6 +19,7 @@ fn main() {
 
     let server = rocket::ignite()
         .mount("/", routes![index])
+        .mount("/", StaticFiles::from("static/"))
         .mount("/", routes![hello]);
 
     server.launch();
